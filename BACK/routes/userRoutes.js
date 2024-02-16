50879f0e610e568c7783bbaf04a914dfeb358b68
Finlais <arnaud.le-cozannet@orange.fr>
2024-02-16 09:43:29 +0100

    feat: Update User finished successfully

--- BACK/routes/userRoutes.js
+++ BACK/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getUsers, addUser, deleteUser } from "../controllers/userController";
+import {
+  getUsers,
+  addUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/addUser", addUser);
 router.delete("/deleteUser", deleteUser);
+router.put("/updateUser", updateUser);
 
 module.exports = router;
