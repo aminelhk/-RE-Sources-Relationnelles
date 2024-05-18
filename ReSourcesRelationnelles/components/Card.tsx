90925268714d1c9629a31b7ecdace279e7ad5617
Finlais <arnaud.le-cozannet@orange.fr>
2024-05-18 11:58:28 +0200

    feat: Composant Card & CardList

---
+++ ReSourcesRelationnelles/components/Card.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { TouchableOpacity, View, Image, Text, StyleSheet } from "react-native";
+import FeatherIcon from "react-native-vector-icons/Feather";
+
+type ItemType = {
+  img: string;
+  name: string;
+  price: number;
+  hp: number;
+  acceleration: number;
+  miles: number;
+  location: string;
+  date: Date;
+};
+const Card = (item: ItemType) => {
+  return (
+    <TouchableOpacity
+      style={styles.cardContainer}
+      onPress={() => {
+        // handle onPress
+      }}
+    >
+      <View style={styles.card}>
+        <View style={styles.cardTop}>
+          <Image
+            alt=""
+            resizeMode="cover"
+            style={styles.cardImg}
+            source={{ uri: item.img }}
+          />
+        </View>
+
+        <View style={styles.cardBody}>
+          <View style={styles.cardHeader}>
+            <Text style={styles.cardTitle}>{item.name}</Text>
+            <Text style={styles.cardPrice}>
+              ${item.price && item.price.toLocaleString("en-US")}
+            </Text>
+          </View>
+
+          <View style={styles.cardStats}>
+            <View style={styles.cardStatsItem}>
+              <FeatherIcon color="#48496c" name="zap" size={14} />
+              <Text style={styles.cardStatsItemText}>{item.hp} hp</Text>
+            </View>
+
+            <View style={styles.cardStatsItem}>
+              <FeatherIcon color="#48496c" name="navigation" size={14} />
+              <Text style={styles.cardStatsItemText}>
+                {item.miles.toLocaleString("en-US")} miles
+              </Text>
+            </View>
+
+            <View style={styles.cardStatsItem}>
+              <FeatherIcon color="#48496c" name="clock" size={14} />
+              <Text style={styles.cardStatsItemText}>
+                {item.acceleration} sec
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.cardFooter}>
+            <Text style={styles.cardFooterText}>{item.location}</Text>
+            <Text style={styles.cardFooterText}>
+              {item.date.toLocaleDateString("en-US", {
+                day: "numeric",
+                year: "numeric",
+                month: "short",
+              })}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    flex: 1,
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    borderRadius: 12,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  cardTop: {
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    overflow: "hidden",
+  },
+  cardImg: {
+    width: "100%",
+    height: 180,
+  },
+  cardBody: {
+    paddingVertical: 16,
+    paddingHorizontal: 12,
+  },
+  cardHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 12,
+  },
+  cardTitle: {
+    fontSize: 19,
+    fontWeight: "600",
+    color: "#2d2d2d",
+  },
+  cardPrice: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#444",
+  },
+  cardStats: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 12,
+  },
+  cardStatsItem: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cardStatsItemText: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#48496c",
+    marginLeft: 4,
+  },
+  cardFooter: {
+    borderTopWidth: 1,
+    borderColor: "#e9e9e9",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 8,
+  },
+  cardFooterText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#909090",
+  },
+});
+
+export default Card;
---
+++ ReSourcesRelationnelles/components/CardList.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import { StyleSheet, SafeAreaView, FlatList } from "react-native";
+import Card from "./Card";
+
+const items = [
+  {
+    img: "https://images.unsplash.com/photo-1623659248894-1a0272243054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2405&q=80",
+    name: "Audi R8",
+    price: 158600,
+    hp: 562,
+    acceleration: 3.2,
+    miles: 24000,
+    location: "Seattle, WA",
+    date: new Date("2022-10-20"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1590656364826-5f13b8e32cdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80",
+    name: "Nissan GTR",
+    price: 225500,
+    hp: 598,
+    acceleration: 3.7,
+    miles: 13000,
+    location: "Richmond, VA",
+    date: new Date("2022-11-22"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+    name: "Porsche 911",
+    price: 160100,
+    hp: 640,
+    acceleration: 3.5,
+    miles: 6000,
+    location: "San Diego, CA",
+    date: new Date("2022-11-23"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1623659248894-1a0272243054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2405&q=80",
+    name: "Audi R8",
+    price: 158600,
+    hp: 562,
+    acceleration: 3.2,
+    miles: 24000,
+    location: "Seattle, WA",
+    date: new Date("2022-10-20"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1590656364826-5f13b8e32cdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80",
+    name: "Nissan GTR",
+    price: 225500,
+    hp: 598,
+    acceleration: 3.7,
+    miles: 13000,
+    location: "Richmond, VA",
+    date: new Date("2022-11-22"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+    name: "Porsche 911",
+    price: 160100,
+    hp: 640,
+    acceleration: 3.5,
+    miles: 6000,
+    location: "San Diego, CA",
+    date: new Date("2022-11-23"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1623659248894-1a0272243054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2405&q=80",
+    name: "Audi R8",
+    price: 158600,
+    hp: 562,
+    acceleration: 3.2,
+    miles: 24000,
+    location: "Seattle, WA",
+    date: new Date("2022-10-20"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1590656364826-5f13b8e32cdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80",
+    name: "Nissan GTR",
+    price: 225500,
+    hp: 598,
+    acceleration: 3.7,
+    miles: 13000,
+    location: "Richmond, VA",
+    date: new Date("2022-11-22"),
+  },
+  {
+    img: "https://images.unsplash.com/photo-1580274455191-1c62238fa333?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+    name: "Porsche 911",
+    price: 160100,
+    hp: 640,
+    acceleration: 3.5,
+    miles: 6000,
+    location: "San Diego, CA",
+    date: new Date("2022-11-23"),
+  },
+];
+
+const CardList = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <Card item={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={4} // Number of columns
+        contentContainerStyle={styles.flatListContent}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#f2f2f2",
+  },
+  flatListContent: {
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+});
+
+export default CardList;
